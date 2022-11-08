4570cad79df8cc535865cbecce9f0925fdd1fbde
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2022-11-08 23:53:13 +0530

    Update make_the_string_great.py

--- November/Day8/make_the_string_great.py
+++ November/Day8/make_the_string_great.py
@@ -55,3 +55,18 @@ class Solution:
             
         return "".join(stack)
         
+# Easy String
+class Solution:
+    def makeGood(self, s: str) -> str:
+        st = []
+        for i in s:
+            if len(st) == 0:
+                st.append(i)
+            else:
+                if st[-1].lower() == i.lower() and st[-1] != i:
+                    st.pop()
+                else:
+                    st.append(i)
+                    
+        # print(st)
+        return "".join(st)
